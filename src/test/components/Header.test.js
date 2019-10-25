d776c304c21b59d152129bad966a79c73d6592f9
dwikiarlan <arrlancore@gmail.com>
2019-10-25 22:17:24 +0700

    add header test

---
+++ src/test/components/Header.test.js
@@ -0,0 +1,16 @@
+import {render} from '../utils'
+import Header from '../../components/Header'
+import React from 'react'
+
+describe('<Header /> component', () => {
+    it('renders with brand name "conduit"', () => {
+        const wrapper = render(<Header />)
+        expect(wrapper.getByText('conduit')).toBeDefined()
+      })
+    it('renders with 3 navbar item', () => {
+      const wrapper = render(<Header />)
+      const navItems= wrapper.container.getElementsByClassName('nav-item')
+      expect(navItems.length).toEqual(3)
+      expect(wrapper.asFragment()).toMatchSnapshot()
+    })
+})
